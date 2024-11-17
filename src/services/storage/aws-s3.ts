ea5675639d2fabf21d4835a4f7edc28a39791b7b
Antoine <zenocode@protonmail.com>
2024-11-17 23:55:43 +0100

    Force URL for S3Client

--- src/services/storage/aws-s3.ts
+++ src/services/storage/aws-s3.ts
@@ -5,25 +5,28 @@ import {
   ListObjectsCommand,
   PutObjectCommand,
   ObjectCannedACL,
-} from '@aws-sdk/client-s3';
-import { StorageListResponse, generateStorageId } from '.';
+} from "@aws-sdk/client-s3";
+import {StorageListResponse, generateStorageId} from ".";
 
-const AWS_S3_BUCKET = process.env.NEXT_PUBLIC_AWS_S3_BUCKET ?? '';
-const AWS_S3_REGION = process.env.NEXT_PUBLIC_AWS_S3_REGION ?? '';
-const AWS_S3_ACCESS_KEY = process.env.AWS_S3_ACCESS_KEY ?? '';
-const AWS_S3_SECRET_ACCESS_KEY = process.env.AWS_S3_SECRET_ACCESS_KEY ?? '';
+const AWS_S3_BUCKET = process.env.NEXT_PUBLIC_AWS_S3_BUCKET ?? "";
+const AWS_S3_REGION = process.env.NEXT_PUBLIC_AWS_S3_REGION ?? "";
+const AWS_S3_ACCESS_KEY = process.env.AWS_S3_ACCESS_KEY ?? "";
+const AWS_S3_SECRET_ACCESS_KEY = process.env.AWS_S3_SECRET_ACCESS_KEY ?? "";
 
-export const AWS_S3_BASE_URL = AWS_S3_BUCKET && AWS_S3_REGION
-  ? `https://${AWS_S3_BUCKET}.s3.${AWS_S3_REGION}.io.cloud.ovh.net`
-  : undefined;
+export const AWS_S3_BASE_URL =
+  AWS_S3_BUCKET && AWS_S3_REGION
+    ? `https://${AWS_S3_BUCKET}.s3.${AWS_S3_REGION}.io.cloud.ovh.net`
+    : undefined;
 
-export const awsS3Client = () => new S3Client({
-  region: AWS_S3_REGION,
-  credentials: {
-    accessKeyId: AWS_S3_ACCESS_KEY,
-    secretAccessKey: AWS_S3_SECRET_ACCESS_KEY,
-  },
-});
+export const awsS3Client = () =>
+  new S3Client({
+    endpoint: AWS_S3_BASE_URL,
+    region: AWS_S3_REGION,
+    credentials: {
+      accessKeyId: AWS_S3_ACCESS_KEY,
+      secretAccessKey: AWS_S3_SECRET_ACCESS_KEY,
+    },
+  });
 
 const urlForKey = (key?: string) => `${AWS_S3_BASE_URL}/${key}`;
 
@@ -31,58 +34,71 @@ export const isUrlFromAwsS3 = (url?: string) =>
   AWS_S3_BASE_URL && url?.startsWith(AWS_S3_BASE_URL);
 
 export const awsS3PutObjectCommandForKey = (Key: string) =>
-  new PutObjectCommand({ Bucket: AWS_S3_BUCKET, Key, ACL: 'public-read' });
+  new PutObjectCommand({Bucket: AWS_S3_BUCKET, Key, ACL: "public-read"});
 
 export const awsS3Put = async (
   file: Buffer,
   fileName: string,
-  acl: ObjectCannedACL = 'public-read'
+  acl: ObjectCannedACL = "public-read"
 ): Promise<string> =>
-  awsS3Client().send(new PutObjectCommand({
-    Bucket: AWS_S3_BUCKET,
-    Key: fileName,
-    Body: file,
-    ACL: acl,
-  }))
+  awsS3Client()
+    .send(
+      new PutObjectCommand({
+        Bucket: AWS_S3_BUCKET,
+        Key: fileName,
+        Body: file,
+        ACL: acl,
+      })
+    )
     .then(() => urlForKey(fileName));
 
 export const awsS3Copy = async (
   fileNameSource: string,
   fileNameDestination: string,
   addRandomSuffix?: boolean,
-  acl: ObjectCannedACL = 'public-read'
+  acl: ObjectCannedACL = "public-read"
 ) => {
-  const name = fileNameSource.split('.')[0];
-  const extension = fileNameSource.split('.')[1];
+  const name = fileNameSource.split(".")[0];
+  const extension = fileNameSource.split(".")[1];
   const Key = addRandomSuffix
     ? `${name}-${generateStorageId()}.${extension}`
     : fileNameDestination;
 
-  return awsS3Client().send(new CopyObjectCommand({
-    Bucket: AWS_S3_BUCKET,
-    CopySource: AWS_S3_BUCKET+fileNameSource.replace(AWS_S3_BASE_URL!, ""),
-    Key,
-    ACL: acl,
-  }))
+  return awsS3Client()
+    .send(
+      new CopyObjectCommand({
+        Bucket: AWS_S3_BUCKET,
+        CopySource:
+          AWS_S3_BUCKET + fileNameSource.replace(AWS_S3_BASE_URL!, ""),
+        Key,
+        ACL: acl,
+      })
+    )
     .then(() => urlForKey(fileNameDestination));
 };
 
-export const awsS3List = async (
-  Prefix: string,
-): Promise<StorageListResponse> =>
-  awsS3Client().send(new ListObjectsCommand({
-    Bucket: AWS_S3_BUCKET,
-    Prefix,
-  }))
-    .then((data) => data.Contents?.map(({ Key, LastModified }) => ({
-      url: urlForKey(Key),
-      fileName: Key ?? '',
-      uploadedAt: LastModified,
-    })) ?? []);
+export const awsS3List = async (Prefix: string): Promise<StorageListResponse> =>
+  awsS3Client()
+    .send(
+      new ListObjectsCommand({
+        Bucket: AWS_S3_BUCKET,
+        Prefix,
+      })
+    )
+    .then(
+      data =>
+        data.Contents?.map(({Key, LastModified}) => ({
+          url: urlForKey(Key),
+          fileName: Key ?? "",
+          uploadedAt: LastModified,
+        })) ?? []
+    );
 
 export const awsS3Delete = async (Key: string) => {
-  awsS3Client().send(new DeleteObjectCommand({
-    Bucket: AWS_S3_BUCKET,
-    Key,
-  }));
+  awsS3Client().send(
+    new DeleteObjectCommand({
+      Bucket: AWS_S3_BUCKET,
+      Key,
+    })
+  );
 };
